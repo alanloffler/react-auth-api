808b30ade730c80cae9e319d57d3d1d5677387fc
alanloffler <alanmatiasloffler@gmail.com>
2025-11-19 20:49:02 -0300

    feat(app): routes and toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Login } from './features/login/Login'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Home } from "@/features/home/Home";
+import { Login } from "@login/Login";
+import { Toaster } from "@components/ui/sonner";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  ]);
 
   return (
-    <div className='w-full'>
-      <Login />
-    </div>
-  )
+    <>
+      <RouterProvider router={router} />
+      <Toaster position="bottom-center" richColors theme="light" />
+    </>
+  );
 }
 
-export default App
+export default App;
