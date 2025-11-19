58ec73a20b61a26fb4de0afc08a05c2d3518209f
alanloffler <alanmatiasloffler@gmail.com>
2025-11-19 20:47:02 -0300

    feat(login): styles

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -2,10 +2,8 @@ import { LoginForm } from "./components/LoginForm";
 
 export function Login() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10 min-w-4xl">
-      <div className="w-full h-full">
-        <LoginForm />
-      </div>
+    <div className="min-h-screen w-full flex items-center justify-center p-4 sm:p-6 md:p-10">
+      <LoginForm />
     </div>
   );
 }
