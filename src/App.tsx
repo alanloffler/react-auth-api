f1aac959b0574bc0b680233786f59e321f1e822e
alanloffler <alanmatiasloffler@gmail.com>
2025-11-22 10:53:33 -0300

    feat(app): add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import { Home } from "@/features/home/Home";
 import { Login } from "@login/Login";
 import { Toaster } from "@components/ui/sonner";
+import { Users } from "@/features/users/Users";
+import { Roles } from "./features/roles/Roles";
+import { Dashboard } from "./features/dashboard/Dashboard";
+// import { Roles } from "@/features/users/Users";
 
 function App() {
   const router = createBrowserRouter([
@@ -12,6 +16,20 @@ function App() {
     {
       path: "/home",
       element: <Home />,
+      children: [
+        {
+          path: "/home",
+          element: <Dashboard />,
+        },
+        {
+          path: "/home/users",
+          element: <Users />,
+        },
+        {
+          path: "/home/roles",
+          element: <Roles />,
+        },
+      ],
     },
   ]);
 
