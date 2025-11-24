38208533ceffdb68468e104b0a6505a9435367c3
alanloffler <alanmatiasloffler@gmail.com>
2025-11-23 21:14:54 -0300

    feat(sidebar): update with logged admin data

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -4,7 +4,6 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@components/ui/dropdown-menu";
@@ -12,17 +11,11 @@ import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@co
 import axios from "axios";
 import { AuthAPI } from "@/core/auth/auth.service";
 import { toast } from "sonner";
+import { useAuthStore } from "@core/auth/auth.store";
 import { useNavigate } from "react-router";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-}) {
+export function NavUser() {
+  const { admin } = useAuthStore();
   const { isMobile } = useSidebar();
   const navigate = useNavigate();
 
@@ -54,8 +47,8 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{`@${admin?.userName}`}</span>
+                <span className="truncate text-xs">{admin?.role.name}</span>
               </div>
               <Ellipsis className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -66,16 +59,6 @@ export function NavUser({
             align="end"
             sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
-                </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-
             <DropdownMenuGroup>
               <DropdownMenuItem>
                 <BadgeCheck />
