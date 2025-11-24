21093aa78d5bcdc31c70f553c25a36b1abcd6dac
alanloffler <alanmatiasloffler@gmail.com>
2025-11-23 21:27:38 -0300

    refactor(auth): update api paths

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { apiClient } from "@core/client/client";
+import type { IAdmin } from "@admin/interfaces/admin.interface";
 import type { IApiResponse } from "@core/interfaces/api-response.interface";
 import type { ICredentials, ISignIn } from "@core/interfaces/auth.interface";
+import { apiClient } from "@core/client/client";
 
 class AuthService {
   private static instance: AuthService;
@@ -14,12 +15,17 @@ class AuthService {
   }
 
   public async signIn(credentials: ICredentials): Promise<IApiResponse<ISignIn>> {
-    const response = await apiClient.post<IApiResponse<ISignIn>>("/signIn", credentials);
+    const response = await apiClient.post<IApiResponse<ISignIn>>("/auth/signIn", credentials);
     return response.data;
   }
 
   public async signOut(): Promise<IApiResponse<null>> {
-    const response = await apiClient.get("/signOut");
+    const response = await apiClient.get("/auth/signOut");
+    return response.data;
+  }
+
+  public async getAdmin(): Promise<IApiResponse<IAdmin>> {
+    const response = await apiClient.get<IApiResponse<IAdmin>>("/auth/admin");
     return response.data;
   }
 }
