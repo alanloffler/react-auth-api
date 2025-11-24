f52e08ad70f9cbfdfbbdeccb4a15fc698cb34502
alanloffler <alanmatiasloffler@gmail.com>
2025-11-23 21:20:19 -0300

    refactor(home): styles

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -33,7 +33,7 @@ export function Home() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-8 pt-0">
           <Outlet />
           {/*<div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="bg-muted/50 aspect-video rounded-xl" />
