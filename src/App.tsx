d203bad7dbcc25b8049aa12ce74190d885851f8c
alanloffler <alanmatiasloffler@gmail.com>
2025-11-29 17:25:43 -0300

    refactor(app): roles and paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,57 @@
 import { Admin } from "@admin/Admin";
+import { AppInitializer } from "@core/auth/components/AppInitializer";
 import { Dashboard } from "@/features/dashboard/Dashboard";
 import { GuestRoute } from "@core/auth/components/GuestRoute";
-import { Home } from "@/features/home/Home";
 import { Login } from "@login/Login";
+import { ProtectedLayout } from "@core/auth/components/ProtectedLayout";
+import { ProtectedRoute } from "@core/auth/components/ProtectedRoute";
 import { Roles } from "@/features/roles/Roles";
 import { Toaster } from "@components/ui/sonner";
-// import { Roles } from "@/features/users/Users";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <GuestRoute>
-          <Login />
-        </GuestRoute>
-      ),
-    },
-    {
-      path: "/home",
-      element: <Home />,
-      children: [
-        {
-          path: "/home",
-          element: <Dashboard />,
-        },
-        {
-          path: "/home/admin",
-          element: <Admin />,
-        },
-        {
-          path: "/home/roles",
-          element: <Roles />,
-        },
-      ],
-    },
-  ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <GuestRoute>
+        <Login />
+      </GuestRoute>
+    ),
+  },
+  {
+    path: "/",
+    element: <ProtectedLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "admin",
+        element: (
+          <ProtectedRoute allowedRoles={["superadmin", "admin"]}>
+            <Admin />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "roles",
+        element: (
+          <ProtectedRoute allowedRoles={["superadmin", "admin"]}>
+            <Roles />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
 
+export default function App() {
   return (
-    <>
+    <AppInitializer>
       <RouterProvider router={router} />
       <Toaster position="bottom-center" richColors theme="light" />
-    </>
+    </AppInitializer>
   );
 }
-
-export default App;
