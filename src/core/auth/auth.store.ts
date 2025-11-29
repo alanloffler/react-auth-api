e6fd24244d4203a25b4a65c979fa8dacfa87a0fd
alanloffler <alanmatiasloffler@gmail.com>
2025-11-29 16:46:18 -0300

    refactor(auth): loadingAdmin value

--- src/core/auth/auth.store.ts
+++ src/core/auth/auth.store.ts
@@ -1,13 +1,12 @@
+import type { IAdmin } from "@admin/interfaces/admin.interface";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-import type { IAdmin } from "@admin/interfaces/admin.interface";
-
 interface AuthState {
   admin?: IAdmin;
-  setAdmin: (admin?: IAdmin) => void;
-  clearAdmin: () => void;
   loadingAdmin: boolean;
+  clearAdmin: () => void;
+  setAdmin: (admin?: IAdmin) => void;
   setLoadingAdmin: (loading: boolean) => void;
 }
 
@@ -15,10 +14,10 @@ export const useAuthStore = create(
   persist<AuthState>(
     (set) => ({
       admin: undefined,
-      setAdmin: (admin) => set({ admin }),
-      clearAdmin: () => set({ admin: undefined }),
+      loadingAdmin: false,
 
-      loadingAdmin: true,
+      clearAdmin: () => set({ admin: undefined }),
+      setAdmin: (admin) => set({ admin }),
       setLoadingAdmin: (loading) => set({ loadingAdmin: loading }),
     }),
     { name: "admin" },
