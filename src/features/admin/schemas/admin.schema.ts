bd5634cecaa9d4d384903b92476596c4cb8c97a1
alanloffler <alanmatiasloffler@gmail.com>
2025-11-29 16:41:14 -0300

    feat(admin): new schema for admin

---
+++ src/features/admin/schemas/admin.schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const adminSchema = z.object({
+  email: z.email({ message: "Debes ingresar un email válido" }),
+  firstName: z.string().nonempty("El nombre no puede estar vacío"),
+  ic: z.string().nonempty("El número de DNI no puede estar vacío"),
+  lastName: z.string().nonempty("El apellido no puede estar vacío"),
+  password: z
+    .string()
+    .nonempty("La contraseña no puede estar vacía")
+    .min(8, "La contraseña debe tener al menos 8 caracteres"),
+  phoneNumber: z.string().nonempty("El número de teléfono no puede estar vacío"),
+  roleId: z.string().nonempty("El rol no puede estar vacío"),
+  userName: z.string().nonempty("El nombre de usuario no puede estar vacío"),
+});
