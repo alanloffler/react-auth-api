172d0f6682993f9fc06f5e965e663b194d91aac4
alanloffler <alanmatiasloffler@gmail.com>
2025-12-04 22:24:20 -0300

    styles(page-header): remove padding top

--- src/components/pages/PageHeader.tsx
+++ src/components/pages/PageHeader.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 export function PageHeader({ children, title, subtitle }: IProps) {
   return (
-    <div className="flex items-center justify-between pt-8">
+    <div className="flex items-center justify-between">
       <div className="flex flex-col">
         <h1 className="text-lg font-semibold">{title}</h1>
         <p className="text-sm font-normal">{subtitle}</p>
