999ff285c477d5707a97ab797896f9a04a637d41
alanloffler <alanmatiasloffler@gmail.com>
2025-12-05 14:54:45 -0300

    feat(admin): data & actions on admin view

--- src/features/admin/views/ViewAdmin.tsx
+++ src/features/admin/views/ViewAdmin.tsx
@@ -1,10 +1,14 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Link } from "react-router";
+
+import type { IAdmin } from "@admin/interfaces/admin.interface";
+import { AdminService } from "@admin/services/admin.service";
+import { toast } from "sonner";
 import { tryCatch } from "@/core/utils/try-catch";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { AdminService } from "../services/admin.service";
-import type { IAdmin } from "../interfaces/admin.interface";
-import { toast } from "sonner";
 
 export default function ViewAdmin() {
   const [admin, setAdmin] = useState<IAdmin | undefined>(undefined);
@@ -29,27 +33,43 @@ export default function ViewAdmin() {
   }, [id]);
 
   return (
-    <Card className="w-full lg:w-[80%] xl:w-[60%]">
+    <Card className="relative w-fit p-10 text-center">
+      <Button className="absolute top-5 right-5" variant="ghost" size="icon-lg" asChild>
+        <Link to="/admin">
+          <ArrowLeft className="size-5 cursor-pointer" />
+        </Link>
+      </Button>
       <CardHeader>
-        <CardTitle className="text-lg">{`${admin?.firstName} ${admin?.lastName}`}</CardTitle>
-        <CardDescription>{admin?.role.name}</CardDescription>
+        <CardTitle className="text-xl">{`${admin?.firstName} ${admin?.lastName}`}</CardTitle>
+        <CardDescription className="text-base">{admin?.role.name}</CardDescription>
       </CardHeader>
-      <CardContent className="flex-1">
-        <ul>
-          <li className="flex gap-2">
-            <span className="font-bold">DNI</span>
+      <CardContent className="min-w-80 flex-1 space-y-6 px-0">
+        <ul className="space-y-2">
+          <li className="flex justify-between">
+            <span className="font-semibold">Usuario</span>
+            <span>{admin?.userName}</span>
+          </li>
+          <li className="flex justify-between">
+            <span className="font-semibold">DNI</span>
             <span>{admin?.ic}</span>
           </li>
-          <li className="flex gap-2">
-            <span className="font-bold">E-mail</span>
+          <li className="flex justify-between">
+            <span className="font-semibold">E-mail</span>
             <span>{admin?.email}</span>
           </li>
-          <li className="flex gap-2">
-            <span className="font-bold">Telefono</span>
-            <span>{admin?.email}</span>
+          <li className="flex justify-between">
+            <span className="font-semibold">Teléfono</span>
+            <span>{admin?.phoneNumber}</span>
           </li>
         </ul>
+        <p className="text-muted-foreground px-0">{`Usuario desde el ${admin && new Date(admin.createdAt.split("T")[0]).toLocaleDateString()}`}</p>
       </CardContent>
+      <CardFooter className="justify-end! gap-3 px-0">
+        <Button variant="outline" asChild>
+          <Link to={`/admin/edit/${id}`}>Editar</Link>
+        </Button>
+        <Button variant="destructive">Eliminar</Button>
+      </CardFooter>
     </Card>
   );
 }
