64271aeb5dc9198af88fa609992ba3ff52c7ad8f
alanloffler <alanmatiasloffler@gmail.com>
2025-12-04 21:44:13 -0300

    feat(dashboard): quick action cards

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,7 +1,40 @@
+import { Shield, ShieldPlus, UserRoundPlus, UsersRound } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { Link } from "react-router";
+import { PageHeader } from "@/components/pages/PageHeader";
+
 export function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <PageHeader title="Panel de control" subtitle="Administra tu aplicación" />
+      <div className="mt-10 grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
+        <Link className="col-span-2" to="/admin/create">
+          <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
+            <UsersRound className="h-6 w-6" />
+            <span>Administradores</span>
+          </Card>
+        </Link>
+        <Link className="col-span-2" to="/admin/create">
+          <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
+            <UserRoundPlus className="h-6 w-6" />
+            <span>Crear administrador</span>
+          </Card>
+        </Link>
+      </div>
+      <div className="mt-10 grid grid-cols-8 gap-8">
+        <Link className="col-span-2" to="/roles">
+          <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
+            <Shield className="h-6 w-6" />
+            <span>Roles</span>
+          </Card>
+        </Link>
+        <Link className="col-span-2" to="/roles/create">
+          <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
+            <ShieldPlus className="h-6 w-6" />
+            <span>Crear rol</span>
+          </Card>
+        </Link>
+      </div>
     </div>
   );
 }
