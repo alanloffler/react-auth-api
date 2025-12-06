6859a012266a056752dccb177efc8e5de006843e
alanloffler <alanmatiasloffler@gmail.com>
2025-12-06 20:50:50 -0300

    refactor(auth): roles enum on forbidden component

--- src/core/auth/components/Forbidden.tsx
+++ src/core/auth/components/Forbidden.tsx
@@ -1,19 +1,20 @@
 import type { ReactNode } from "react";
 
+import type { TRole } from "@auth/enums/role.enum";
 import { useAuthStore } from "@auth/auth.store";
 
 type TVariant = "disabled" | "invisible";
 
 interface IProps {
   children: ReactNode;
-  to: string[];
+  to: TRole[];
   variant?: TVariant;
 }
 
 export function Forbidden({ children, to, variant = "disabled" }: IProps) {
   const admin = useAuthStore((state) => state.admin);
 
-  if (admin && to.includes(admin.role.value)) {
+  if (admin && to.includes(admin.role.value as TRole)) {
     if (variant === "disabled") {
       return <div className="pointer-events-none cursor-pointer opacity-50">{children}</div>;
     }
