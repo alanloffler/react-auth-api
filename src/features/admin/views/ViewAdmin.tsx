1ce3114c997ecbf578f056c03f0eb5f0f54488ff
alanloffler <alanmatiasloffler@gmail.com>
2025-12-06 23:02:57 -0300

    feat(admin): find admin by role, handle loading state

--- src/features/admin/views/ViewAdmin.tsx
+++ src/features/admin/views/ViewAdmin.tsx
@@ -5,78 +5,102 @@ import { Forbidden } from "@auth/components/Forbidden";
 import { Link } from "react-router";
 
 import { toast } from "sonner";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 import type { IAdmin } from "@admin/interfaces/admin.interface";
 import { AdminService } from "@admin/services/admin.service";
 import { ERoles } from "@auth/enums/role.enum";
 import { tryCatch } from "@core/utils/try-catch";
+import { useAuthStore } from "@auth/auth.store";
 
 export default function ViewAdmin() {
   const [admin, setAdmin] = useState<IAdmin | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const adminAuth = useAuthStore((state) => state.admin);
   const { id } = useParams();
 
-  async function findOneAdmin(id: string) {
-    const [response, responseError] = await tryCatch(AdminService.findOne(id!));
+  const findOneAdmin = useCallback(
+    async function (id: string) {
+      setIsLoading(true);
+      const isSuperAdmin = adminAuth?.role.value === ERoles.SUPER;
+      const serviceByRole = isSuperAdmin ? AdminService.findOneSoftRemoved(id) : AdminService.findOne(id);
 
-    if (responseError) {
-      toast.error(responseError.message);
+      const [response, responseError] = await tryCatch(serviceByRole);
 
-      return;
-    }
+      setIsLoading(false);
 
-    if (response && response.statusCode === 200) {
-      setAdmin(response.data);
-    }
-  }
+      if (responseError) {
+        toast.error(responseError.message);
+        return;
+      }
+
+      if (response && response.statusCode === 200) {
+        setAdmin(response.data);
+      }
+    },
+    [adminAuth?.role.value],
+  );
 
   useEffect(() => {
     findOneAdmin(id!);
-  }, [id]);
+  }, [id, findOneAdmin]);
 
   return (
     <Card className="relative w-fit p-10 text-center">
-      <Button className="absolute top-5 right-5" variant="ghost" size="icon-lg" asChild>
-        <Link to="/admin">
-          <ArrowLeft className="size-5 cursor-pointer" />
-        </Link>
-      </Button>
-      <CardHeader>
-        <CardTitle className="text-xl">{`${admin?.firstName} ${admin?.lastName}`}</CardTitle>
-        <CardDescription className="text-base">{admin?.role.name}</CardDescription>
-      </CardHeader>
-      <CardContent className="min-w-80 flex-1 space-y-6 px-0">
-        <ul className="space-y-2">
-          <li className="flex justify-between">
-            <span className="font-semibold">Usuario</span>
-            <span>{admin?.userName}</span>
-          </li>
-          <li className="flex justify-between">
-            <span className="font-semibold">DNI</span>
-            <span>{admin?.ic}</span>
-          </li>
-          <li className="flex justify-between">
-            <span className="font-semibold">E-mail</span>
-            <span>{admin?.email}</span>
-          </li>
-          <li className="flex justify-between">
-            <span className="font-semibold">Teléfono</span>
-            <span>{admin?.phoneNumber}</span>
-          </li>
-        </ul>
-        <p className="text-muted-foreground px-0">{`Usuario desde el ${admin && new Date(admin.createdAt.split("T")[0]).toLocaleDateString()}`}</p>
-      </CardContent>
-      <CardFooter className="justify-end! gap-3 px-0">
-        <Button variant="outline" asChild>
-          <Link to={`/admin/edit/${id}`}>Editar</Link>
-        </Button>
-        <Forbidden to={[ERoles.TEACHER]} variant="invisible">
-          <Button variant="destructive" onClick={() => console.log("eliminar")}>
-            Eliminar
+      {isLoading ? (
+        <div className="min-w-80">Cargando...</div>
+      ) : (
+        <>
+          <Button className="absolute top-5 right-5" variant="ghost" size="icon-lg" asChild>
+            <Link to="/admin">
+              <ArrowLeft className="size-5 cursor-pointer" />
+            </Link>
           </Button>
-        </Forbidden>
-      </CardFooter>
+          <CardHeader>
+            <CardTitle className="text-xl">{`${admin?.firstName} ${admin?.lastName}`}</CardTitle>
+            <CardDescription className="text-base">{admin?.role.name}</CardDescription>
+          </CardHeader>
+          <CardContent className="min-w-80 flex-1 space-y-6 px-0">
+            <ul className="space-y-2">
+              <li className="flex justify-between">
+                <span className="font-semibold">Usuario</span>
+                <span>{admin?.userName}</span>
+              </li>
+              <li className="flex justify-between">
+                <span className="font-semibold">DNI</span>
+                <span>{admin?.ic}</span>
+              </li>
+              <li className="flex justify-between">
+                <span className="font-semibold">E-mail</span>
+                <span>{admin?.email}</span>
+              </li>
+              <li className="flex justify-between">
+                <span className="font-semibold">Teléfono</span>
+                <span>{admin?.phoneNumber}</span>
+              </li>
+            </ul>
+            <p className="text-muted-foreground px-0">{`Usuario desde el ${admin && new Date(admin.createdAt.split("T")[0]).toLocaleDateString()}`}</p>
+          </CardContent>
+
+          <Forbidden to={[ERoles.TEACHER]} variant="invisible">
+            <CardFooter className="justify-end gap-3 px-0">
+              {admin?.deletedAt && admin?.deletedAt !== null ? (
+                <Button variant="outline">Restaurar</Button>
+              ) : (
+                <>
+                  <Button variant="outline" asChild>
+                    <Link to={`/admin/edit/${id}`}>Editar</Link>
+                  </Button>
+                  <Button variant="destructive" onClick={() => console.log("eliminar")}>
+                    Eliminar
+                  </Button>
+                </>
+              )}
+            </CardFooter>
+          </Forbidden>
+        </>
+      )}
     </Card>
   );
 }
