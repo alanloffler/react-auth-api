3946a89ebe9bf6ea3b3d5c0a710a9c50e01b2f88
alanloffler <alanmatiasloffler@gmail.com>
2025-12-08 00:05:52 -0300

    feat(roles): validate permissions schema

--- src/features/roles/schemas/role.schema.ts
+++ src/features/roles/schemas/role.schema.ts
@@ -1,7 +1,27 @@
-import { z } from "zod";
+import z from "zod";
+
+const actionSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  key: z.string(),
+  value: z.boolean(),
+});
+
+const permissionSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  module: z.string(),
+  actions: z.array(actionSchema),
+});
 
 export const roleSchema = z.object({
   description: z.string().nonempty("La descripción no puede estar vacía"),
   name: z.string().nonempty("El nombre no puede estar vacío"),
   value: z.string().nonempty("El valor no puede estar vacío"),
+  permissions: z
+    .array(permissionSchema)
+    .refine(
+      (perms) => perms.some((p) => p.actions.some((a) => a.value === true)),
+      "Debe seleccionar al menos un permiso",
+    ),
 });
