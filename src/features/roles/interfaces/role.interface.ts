a9bb8834439bb5e7e20f89f84763fd47a3f1e05d
alanloffler <alanmatiasloffler@gmail.com>
2025-12-09 11:47:38 -0300

    feat(roles): role permissions interface

--- src/features/roles/interfaces/role.interface.ts
+++ src/features/roles/interfaces/role.interface.ts
@@ -1,7 +1,19 @@
+import type { IAdmin } from "@admin/interfaces/admin.interface";
+import type { IPermission } from "@permissions/interfaces/permission.interface";
+
 export interface IRole {
+  admins: Pick<IAdmin, "id" | "firstName" | "lastName" | "userName">[];
+  createdAt: string;
   deletedAt?: string;
   description: string;
   id: string;
   name: string;
+  rolePermissions?: IRolePermissions[];
   value: string;
+}
+
+export interface IRolePermissions {
+  roleId: string;
+  permissionId: string;
+  permission: IPermission;
 }
