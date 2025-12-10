f3ab02ff9d343941c329178e53af1ea7f60fd4ff
alanloffler <alanmatiasloffler@gmail.com>
2025-12-10 20:10:27 -0300

    feat(constants): add permission actions

--- src/core/constants/permissions.ts
+++ src/core/constants/permissions.ts
@@ -1,5 +1,6 @@
 export const PERMISSION_CATEGORIES = [
   { name: "Admin", value: "admin" },
+  { name: "Permisos", value: "permissions" },
   { name: "Roles", value: "roles" },
 ];
 
@@ -7,6 +8,8 @@ export const PERMISSION_ACTIONS = [
   { name: "Crear", value: "create" },
   { name: "Editar", value: "update" },
   { name: "Eliminar", value: "delete" },
+  { name: "Eliminar permanente", value: "delete-hard" },
+  { name: "Restaurar", value: "restore" },
   { name: "Ver", value: "view" },
   { name: "Test", value: "test" },
 ];
