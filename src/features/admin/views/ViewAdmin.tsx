574cac783996e46502e094686f6432f36cffd8c3
alanloffler <alanmatiasloffler@gmail.com>
2025-12-15 21:33:41 -0300

    feat(admin): page header and badge

--- src/features/admin/views/ViewAdmin.tsx
+++ src/features/admin/views/ViewAdmin.tsx
@@ -1,9 +1,11 @@
 import { ArrowLeft, FilePenLine, RotateCcw, Trash2 } from "lucide-react";
 
 import { Button } from "@components/ui/button";
+import { Badge } from "@components/Badge";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@components/ui/card";
 import { HoldButton } from "@components/ui/HoldButton";
 import { Link } from "react-router";
+import { PageHeader } from "@components/pages/PageHeader";
 import { Protected } from "@auth/components/Protected";
 
 import { useNavigate } from "react-router";
@@ -16,7 +18,7 @@ import { AdminService } from "@admin/services/admin.service";
 import { ERoles } from "@auth/enums/role.enum";
 import { tryCatch } from "@core/utils/try-catch";
 import { useAuthStore } from "@auth/auth.store";
-import { usePermission } from "@/core/hooks/usePermission";
+import { usePermission } from "@core/hooks/usePermission";
 
 export default function ViewAdmin() {
   const [admin, setAdmin] = useState<IAdmin | undefined>(undefined);
@@ -96,76 +98,84 @@ export default function ViewAdmin() {
   }, [id, findOneAdmin]);
 
   return (
-    <Card className="relative w-fit p-10 text-center">
-      {isLoading ? (
-        <div className="min-w-80">Cargando...</div>
-      ) : (
-        <>
-          <Button className="absolute top-5 right-5" variant="ghost" size="icon-lg" onClick={() => navigate(-1)}>
-            <ArrowLeft className="size-5 cursor-pointer" />
-          </Button>
-          <CardHeader>
-            <CardTitle className="text-xl">{`${admin?.firstName} ${admin?.lastName}`}</CardTitle>
-            <CardDescription className="text-base">{admin?.role.name}</CardDescription>
-          </CardHeader>
-          <CardContent className="min-w-80 flex-1 space-y-6 px-0">
-            <ul className="space-y-2">
-              <li className="flex justify-between">
-                <span className="font-semibold">Usuario</span>
-                <span>{admin?.userName}</span>
-              </li>
-              <li className="flex justify-between">
-                <span className="font-semibold">DNI</span>
-                <span>{admin?.ic}</span>
-              </li>
-              <li className="flex justify-between">
-                <span className="font-semibold">E-mail</span>
-                <span>{admin?.email}</span>
-              </li>
-              <li className="flex justify-between">
-                <span className="font-semibold">Teléfono</span>
-                <span>{admin?.phoneNumber}</span>
-              </li>
-            </ul>
-            <p className="text-muted-foreground px-0">{`Usuario desde el ${admin && new Date(admin.createdAt.split("T")[0]).toLocaleDateString()}`}</p>
-          </CardContent>
-          <Activity mode={hasPermissions ? "visible" : "hidden"}>
-            <CardFooter className="justify-end gap-3 px-0">
-              {admin?.deletedAt && admin?.deletedAt !== null ? (
-                <HoldButton callback={() => id && restoreAdmin(id)} size="icon" type="restore" variant="outline">
-                  <RotateCcw className="h-4 w-4" />
-                </HoldButton>
-              ) : (
-                <>
-                  <Protected requiredPermission="admin-update">
-                    <Button className="px-5!" variant="outline" asChild>
-                      <Link to={`/admin/edit/${id}`}>
-                        <FilePenLine className="h-4 w-4" />
-                      </Link>
-                    </Button>
-                  </Protected>
-                  <Protected requiredPermission="admin-delete">
-                    <HoldButton callback={() => id && removeAdmin(id)} size="icon" type="delete" variant="outline">
-                      <Trash2 className="h-4 w-4" />
+    <section className="flex flex-col gap-10">
+      <PageHeader title="Detalles del administrador" />
+      <Card className="relative w-fit p-10 text-center">
+        {isLoading ? (
+          <div className="min-w-80">Cargando...</div>
+        ) : (
+          <>
+            <Button className="absolute top-5 right-5" variant="ghost" size="icon-lg" onClick={() => navigate(-1)}>
+              <ArrowLeft className="size-5 cursor-pointer" />
+            </Button>
+            <CardHeader>
+              <CardTitle className="text-xl">{`${admin?.firstName} ${admin?.lastName}`}</CardTitle>
+              <CardDescription className="text-base">{admin?.role.name}</CardDescription>
+            </CardHeader>
+            <CardContent className="min-w-80 flex-1 space-y-6 px-0">
+              <ul className="space-y-2">
+                <li className="flex justify-between">
+                  <span className="font-semibold">Usuario</span>
+                  <span>{admin?.userName}</span>
+                </li>
+                <li className="flex justify-between">
+                  <span className="font-semibold">DNI</span>
+                  <span>{admin?.ic}</span>
+                </li>
+                <li className="flex justify-between">
+                  <span className="font-semibold">E-mail</span>
+                  <span>{admin?.email}</span>
+                </li>
+                <li className="flex justify-between">
+                  <span className="font-semibold">Teléfono</span>
+                  <span>{admin?.phoneNumber}</span>
+                </li>
+              </ul>
+              <p className="text-muted-foreground px-0">{`Usuario desde el ${admin && new Date(admin.createdAt.split("T")[0]).toLocaleDateString()}`}</p>
+            </CardContent>
+            <Activity mode={hasPermissions ? "visible" : "hidden"}>
+              <CardFooter className="justify-end gap-3 px-0">
+                {admin?.deletedAt && admin?.deletedAt !== null ? (
+                  <div className="flex w-full items-center justify-between">
+                    <Badge size="small" variant="red">
+                      Eliminado
+                    </Badge>
+                    <HoldButton callback={() => id && restoreAdmin(id)} size="icon" type="restore" variant="outline">
+                      <RotateCcw className="h-4 w-4" />
                     </HoldButton>
-                  </Protected>
-                  <Protected requiredPermission="admin-delete-hard">
-                    <HoldButton
-                      callback={() => id && hardRemoveAdmin(id)}
-                      size="icon"
-                      type="hard-delete"
-                      variant="outline"
-                    >
-                      <Trash2 className="h-4 w-4" />
-                      <span>!</span>
-                    </HoldButton>
-                  </Protected>
-                </>
-              )}
-            </CardFooter>
-          </Activity>
-        </>
-      )}
-    </Card>
+                  </div>
+                ) : (
+                  <>
+                    <Protected requiredPermission="admin-update">
+                      <Button className="px-5!" variant="outline" asChild>
+                        <Link to={`/admin/edit/${id}`}>
+                          <FilePenLine className="h-4 w-4" />
+                        </Link>
+                      </Button>
+                    </Protected>
+                    <Protected requiredPermission="admin-delete">
+                      <HoldButton callback={() => id && removeAdmin(id)} size="icon" type="delete" variant="outline">
+                        <Trash2 className="h-4 w-4" />
+                      </HoldButton>
+                    </Protected>
+                    <Protected requiredPermission="admin-delete-hard">
+                      <HoldButton
+                        callback={() => id && hardRemoveAdmin(id)}
+                        size="icon"
+                        type="hard-delete"
+                        variant="outline"
+                      >
+                        <Trash2 className="h-4 w-4" />
+                        <span>!</span>
+                      </HoldButton>
+                    </Protected>
+                  </>
+                )}
+              </CardFooter>
+            </Activity>
+          </>
+        )}
+      </Card>
+    </section>
   );
 }
