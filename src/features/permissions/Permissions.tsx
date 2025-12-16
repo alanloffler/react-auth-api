400d0564c1c09802ae64c48872a985a1136c72b3
alanloffler <alanmatiasloffler@gmail.com>
2025-12-16 12:59:21 -0300

    feat(permissions): hover color on edit & view btns

--- src/features/permissions/Permissions.tsx
+++ src/features/permissions/Permissions.tsx
@@ -115,7 +115,7 @@ export default function Permissions() {
       id: "actions",
       cell: ({ row }) => (
         <div className="flex justify-end gap-2">
-          <Button className="px-5!" variant="outline" asChild>
+          <Button className="px-5! hover:text-sky-500" variant="outline" asChild>
             <Link to={`/permissions/view/${row.original.id}`}>
               <Eye className="h-4 w-4" />
             </Link>
@@ -134,7 +134,7 @@ export default function Permissions() {
           ) : (
             <>
               <Protected requiredPermission="permissions-update">
-                <Button className="px-5!" variant="outline" asChild>
+                <Button className="px-5! hover:text-green-500" variant="outline" asChild>
                   <Link to={`/permissions/edit/${row.original.id}`}>
                     <FilePenLine className="h4- w-4" />
                   </Link>
