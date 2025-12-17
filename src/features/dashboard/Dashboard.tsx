bf6d65757d5c9b94780072c16dcf971c0ebe7f71
alanloffler <alanmatiasloffler@gmail.com>
2025-12-16 22:04:09 -0300

    refactor(dashboard): icon component replace inline svg

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { Card } from "@/components/ui/card";
 import { Link } from "react-router";
 import { PageHeader } from "@/components/pages/PageHeader";
 import { Protected } from "@/core/auth/components/Protected";
+import { KeyRoundPlus } from "@/components/icons/KeyRoundPlus";
 
 export function Dashboard() {
   return (
@@ -56,43 +57,7 @@ export function Dashboard() {
             <Protected requiredPermission="permissions-create">
               <Link className="col-span-2" to="/permissions/create">
                 <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-                  {/* <KeyRound className="h-6 w-6" /> */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-key-round-icon lucide-key-round"
-                  >
-                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z" />
-                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor" />
-
-                    <g>
-                      <line
-                        x1="18.5"
-                        y1="17.5"
-                        x2="18.5"
-                        y2="25.5"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                      />
-                      <line
-                        x1="15.5"
-                        y1="20.5"
-                        x2="21.5"
-                        y2="20.5"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                      />
-                    </g>
-                  </svg>
+                  <KeyRoundPlus className="h-6 w-6" />
                   <span>Crear permiso</span>
                 </Card>
               </Link>
