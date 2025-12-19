b38e26c6a05685a5d9092aed10468cd012bf9325
alanloffler <alanmatiasloffler@gmail.com>
2025-12-19 00:04:47 -0300

    fix(admin): unexpected symbol

--- src/features/admin/views/CreateAdmin.tsx
+++ src/features/admin/views/CreateAdmin.tsx
@@ -3,7 +3,7 @@ import { CreateForm } from "@admin/components/CreateForm";
 export default function CreateAdmin() {
   return (
     <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[60%]">
-      <CreateForm />;
+      <CreateForm />
     </div>
   );
 }
