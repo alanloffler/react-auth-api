5180d9916ed6455c0a396a2784893f027f59bb10
alanloffler <alanmatiasloffler@gmail.com>
2025-12-20 12:16:05 -0300

    refactor(permissions): gap-8

--- src/features/permissions/Permissions.tsx
+++ src/features/permissions/Permissions.tsx
@@ -169,7 +169,7 @@ export default function Permissions() {
   ];
 
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-8">
       <PageHeader title="Permisos" subtitle="Gestioná los permisos para los roles del sistema">
         <Protected requiredPermission="permissions-create">
           <Button variant="default" size="lg" asChild>
