b46a6d5c2ff8ef33ba5ab7175469763198cd553c
alanloffler <alanmatiasloffler@gmail.com>
2025-12-20 13:30:39 -0300

    styles(sidebar): app logo colors

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -18,8 +18,8 @@ export function TeamSwitcher({ teams }: IProps) {
             size="lg"
             className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground hover:cursor-pointer"
           >
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-              <teams.logo className="size-4" />
+            <div className="bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+              <teams.logo className="size-4 text-white" />
             </div>
             <div className="grid flex-1 text-left text-sm leading-tight">
               <span className="truncate font-medium">{teams.name}</span>
