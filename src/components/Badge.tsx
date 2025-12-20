c495c98321029d1dcfd310e00902029f1875a4b6
alanloffler <alanmatiasloffler@gmail.com>
2025-12-20 12:11:49 -0300

    feat(components): variant

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@lib/utils";
 
-const badgeVariants = cva("inline-flex items-center font-medium uppercase", {
+const badgeVariants = cva("inline-flex items-center justify-center font-medium uppercase", {
   variants: {
     size: {
       normal: "px-3 py-1.5 text-xs rounded-sm",
@@ -13,6 +13,7 @@ const badgeVariants = cva("inline-flex items-center font-medium uppercase", {
       ic: "bg-neutral-200 text-neutral-700 text-xs lowercase font-normal",
       id: "bg-neutral-100 text-neutral-600 text-xs lowercase font-normal",
       gray: "bg-neutral-100 text-neutral-600",
+      green: "bg-green-100 text-green-600 ",
       red: "bg-red-100 text-red-600",
       role: "bg-indigo-100 text-indigo-600",
     },
