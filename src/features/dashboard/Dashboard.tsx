cf643bcf83d56b05c7e4d5ad977f683bffef432c
alanloffler <alanmatiasloffler@gmail.com>
2025-12-20 17:08:13 -0300

    refactor(dashboard): componetize ui

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,69 +1,45 @@
-import { KeyRound, Shield, ShieldPlus, UserRoundPlus, UsersRound } from "lucide-react";
-import { Card } from "@/components/ui/card";
-import { Link } from "react-router";
-import { PageHeader } from "@/components/pages/PageHeader";
-import { Protected } from "@/core/auth/components/Protected";
-import { KeyRoundPlus } from "@/components/icons/KeyRoundPlus";
+import { KeyRound, LayoutDashboard, Package, Shield, ShieldPlus, UserRoundPlus, UsersRound } from "lucide-react";
+
+import { ActionCard } from "@dashboard/components/ActionCard";
+import { ConfigButton } from "@dashboard/components/ConfigButton";
+import { KeyRoundPlus } from "@components/icons/KeyRoundPlus";
+import { PageHeader } from "@components/pages/PageHeader";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="flex flex-col gap-10">
       <PageHeader title="Panel de control" subtitle="Administra tu aplicación" />
-      <div className="mt-10 flex flex-col gap-10">
-        <Protected requiredPermission="admin-view">
-          <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
-            <Link className="col-span-2" to="/admin">
-              <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-                <UsersRound className="h-6 w-6" />
-                <span>Administradores</span>
-              </Card>
-            </Link>
-            <Protected requiredPermission="admin-create">
-              <Link className="col-span-2" to="/admin/create">
-                <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-                  <UserRoundPlus className="h-6 w-6" />
-                  <span>Crear administrador</span>
-                </Card>
-              </Link>
-            </Protected>
-          </div>
-        </Protected>
-        <Protected requiredPermission="roles-view">
-          <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
-            <Link className="col-span-2" to="/roles">
-              <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-                <Shield className="h-6 w-6" />
-                <span>Roles</span>
-              </Card>
-            </Link>
-            <Protected requiredPermission="roles-create">
-              <Link className="col-span-2" to="/roles/create">
-                <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-                  <ShieldPlus className="h-6 w-6" />
-                  <span>Crear rol</span>
-                </Card>
-              </Link>
-            </Protected>
-          </div>
-        </Protected>
-        <Protected requiredPermission="permissions-view">
-          <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
-            <Link className="col-span-2" to="/permissions">
-              <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-                <KeyRound className="h-6 w-6" />
-                <span>Permisos</span>
-              </Card>
-            </Link>
-            <Protected requiredPermission="permissions-create">
-              <Link className="col-span-2" to="/permissions/create">
-                <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-                  <KeyRoundPlus className="h-6 w-6" />
-                  <span>Crear permiso</span>
-                </Card>
-              </Link>
-            </Protected>
-          </div>
-        </Protected>
+      <div className="flex flex-col gap-8">
+        <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
+          <ActionCard icon={UsersRound} permission="admin-view" text="Administradores" url="/admin" />
+          <ActionCard icon={UserRoundPlus} permission="admin-create" text="Crear Administrador" url="/admin/create" />
+        </div>
+        <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
+          <ActionCard icon={Shield} permission="roles-view" text="Roles" url="/roles" />
+          <ActionCard icon={ShieldPlus} permission="roles-create" text="Crear rol" url="/roles/create" />
+        </div>
+        <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
+          <ActionCard icon={KeyRound} permission="permissions-view" text="Permisos" url="/permissions" />
+          <ActionCard
+            icon={KeyRoundPlus}
+            permission="permissions-create"
+            text="Crear permiso"
+            url="/permissions/create"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-semibold">Configuraciones</h1>
+          <ul className="flex gap-3 pl-5">
+            <li>
+              <ConfigButton icon={Package} text="Aplicación" url="/dashboard-settings" />
+            </li>
+            <li>
+              <li>
+                <ConfigButton icon={LayoutDashboard} text="Tablero" url="/dashboard-settings" />
+              </li>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
