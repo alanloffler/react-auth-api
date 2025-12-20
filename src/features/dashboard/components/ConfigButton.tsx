77e4c81db23a72840e46460d31dba91259d7c85b
alanloffler <alanmatiasloffler@gmail.com>
2025-12-20 17:07:50 -0300

    feat(dashboard): config button

---
+++ src/features/dashboard/components/ConfigButton.tsx
@@ -0,0 +1,24 @@
+import { Badge } from "@components/Badge";
+import { Link } from "react-router";
+
+import type { LucideIcon } from "lucide-react";
+
+interface IProps {
+  icon: LucideIcon;
+  text: string;
+  url: string;
+}
+
+export function ConfigButton({ icon: Icon, text, url }: IProps) {
+  return (
+    <Link to={url}>
+      <Badge
+        className="hover:text-foreground uppercase transition-colors duration-150 hover:bg-neutral-200"
+        variant="id"
+      >
+        <Icon className="mr-2 h-4 w-4" strokeWidth={1.5} />
+        {text}
+      </Badge>
+    </Link>
+  );
+}
