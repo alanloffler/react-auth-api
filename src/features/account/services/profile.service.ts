3f2c63e7460040912311cdba4d8ba26d457c689e
alanloffler <alanmatiasloffler@gmail.com>
2025-12-20 22:03:10 -0300

    feat(account): profile service

---
+++ src/features/account/services/profile.service.ts
@@ -0,0 +1,27 @@
+import type { IAdmin } from "@admin/interfaces/admin.interface";
+import type { IApiResponse } from "@core/interfaces/api-response.interface";
+import { apiClient } from "@core/client/client";
+
+class AccountModuleService {
+  private static instance: AccountModuleService;
+
+  public static getInstance(): AccountModuleService {
+    if (!AccountModuleService.instance) {
+      AccountModuleService.instance = new AccountModuleService();
+    }
+
+    return AccountModuleService.instance;
+  }
+
+  public async getMe(): Promise<IApiResponse<IAdmin>> {
+    const response = await apiClient.get("/admin/profile");
+    return response.data;
+  }
+
+  public async update(data: Partial<IAdmin>): Promise<IApiResponse<IAdmin>> {
+    const response = await apiClient.patch("/admin/profile", data);
+    return response.data;
+  }
+}
+
+export const AccountService = AccountModuleService.getInstance();
