487248e86f9a1d920f5978cff13cb3e81dfc4a2a
alanloffler <alanmatiasloffler@gmail.com>
2025-12-22 18:21:26 -0300

    fix(login): layout

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "./components/LoginForm";
 
 export function Login() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center p-4 sm:p-6 md:p-10">
+    <div className="flex h-screen w-full items-center justify-center p-4 sm:p-6 md:p-10">
       <LoginForm />
     </div>
   );
