fa3e629cfbb23057eb1052753c06be4158dc1e04
alanloffler <alanmatiasloffler@gmail.com>
2025-12-22 18:52:23 -0300

    refactor(roles): remove gap

--- src/features/roles/views/EditRole.tsx
+++ src/features/roles/views/EditRole.tsx
@@ -2,7 +2,7 @@ import { EditForm } from "@roles/components/EditForm";
 
 export default function EditRole() {
   return (
-    <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[60%]">
+    <div className="flex w-full flex-col lg:w-[80%] xl:w-[60%]">
       <EditForm />
     </div>
   );
