5f42e931328de975ef55c5ce48d8524f6fbbd01f
alanloffler <alanmatiasloffler@gmail.com>
2025-12-21 21:56:35 -0300

    feat(admin): ic schema min & max

--- src/features/admin/schemas/admin.schema.ts
+++ src/features/admin/schemas/admin.schema.ts
@@ -3,7 +3,11 @@ import { z } from "zod";
 export const adminSchema = z.object({
   email: z.email({ message: "Debes ingresar un email válido" }),
   firstName: z.string().nonempty("El nombre no puede estar vacío"),
-  ic: z.string().nonempty("El número de DNI no puede estar vacío"),
+  ic: z
+    .string()
+    .nonempty("El número de DNI no puede estar vacío")
+    .max(8, "Máximo 8 dígitos")
+    .min(8, "Mínimo 8 dígitos"),
   lastName: z.string().nonempty("El apellido no puede estar vacío"),
   phoneNumber: z.string().nonempty("El número de teléfono no puede estar vacío"),
   roleId: z.string().nonempty("El rol no puede estar vacío"),
