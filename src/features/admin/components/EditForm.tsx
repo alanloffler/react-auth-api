b0c8b44ee1a86074e30bd0c420e172a7950e2709
alanloffler <alanmatiasloffler@gmail.com>
2025-12-23 16:06:22 -0300

    feat(admin): validate equality of ic, username & email before check availability

--- src/features/admin/components/EditForm.tsx
+++ src/features/admin/components/EditForm.tsx
@@ -145,33 +145,39 @@ export function EditForm({ adminId }: IProps) {
     }
 
     // Check again for race condition: before first check another admin use same ic
-    const emailAvailableResponse = await AdminService.checkEmailAvailability(data.email);
+    if (data.email !== adminToUpdate?.email) {
+      const emailAvailableResponse = await AdminService.checkEmailAvailability(data.email);
 
-    if (emailAvailableResponse.data === false) {
-      const errorMsg = "Email ya registrado";
-      setEmailError(errorMsg);
-      form.setError("email", { message: errorMsg });
-      return;
+      if (emailAvailableResponse.data === false) {
+        const errorMsg = "Email ya registrado";
+        setEmailError(errorMsg);
+        form.setError("email", { message: errorMsg });
+        return;
+      }
     }
 
     // Check again for race condition: before first check another admin use same ic
-    const icAvailableResponse = await AdminService.checkIcAvailability(data.ic);
+    if (data.ic !== adminToUpdate?.ic) {
+      const icAvailableResponse = await AdminService.checkIcAvailability(data.ic);
 
-    if (icAvailableResponse.data === false) {
-      const errorMsg = "DNI ya registrado";
-      setIcError(errorMsg);
-      form.setError("ic", { message: errorMsg });
-      return;
+      if (icAvailableResponse.data === false) {
+        const errorMsg = "DNI ya registrado";
+        setIcError(errorMsg);
+        form.setError("ic", { message: errorMsg });
+        return;
+      }
     }
 
     // Check again for race condition: before first check another admin use same username
-    const usernameAvailableResponse = await AdminService.checkUsernameAvailability(data.userName);
+    if (data.userName !== adminToUpdate?.userName) {
+      const usernameAvailableResponse = await AdminService.checkUsernameAvailability(data.userName);
 
-    if (usernameAvailableResponse.data === false) {
-      const errorMsg = "Nombre de usuario ya registrado";
-      setUsernameError(errorMsg);
-      form.setError("userName", { message: errorMsg });
-      return;
+      if (usernameAvailableResponse.data === false) {
+        const errorMsg = "Nombre de usuario ya registrado";
+        setUsernameError(errorMsg);
+        form.setError("userName", { message: errorMsg });
+        return;
+      }
     }
 
     const updateData = data.password
