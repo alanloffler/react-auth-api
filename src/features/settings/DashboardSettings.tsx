da3e7fa501d3a44c8f212d6aa83a21809bf09fbb
alanloffler <alanmatiasloffler@gmail.com>
2025-12-27 18:56:13 -0300

    feat(settings): dashboard content

--- src/features/settings/DashboardSettings.tsx
+++ src/features/settings/DashboardSettings.tsx
@@ -1,5 +1,46 @@
+import { LockKeyhole } from "lucide-react";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
+import { Loader } from "@components/Loader";
 import { PageHeader } from "@components/pages/PageHeader";
+import { Switch } from "@components/ui/switch";
+
+import { usePermission } from "@core/hooks/usePermission";
+import { useSettingsStore } from "@settings/stores/settings.store";
 
 export default function DashboardSettings() {
-  return <PageHeader title="Configuraciones del tablero" />;
+  const canEditSettings = usePermission("settings-update");
+  const { dashboardSettings, loadingDashboardSettings, updateDashboardSetting } = useSettingsStore();
+
+  return (
+    <div className="flex flex-col gap-8">
+      <PageHeader title="Configuraciones de la aplicación" />
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-5">
+        <Card className="relative col-span-1 gap-3 lg:col-span-3 2xl:col-span-2">
+          <CardHeader>
+            <CardTitle>Tablero</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-1 flex-col gap-2">
+            {dashboardSettings.map((setting) => (
+              <div className="flex items-center gap-3" key={setting.id}>
+                <Switch
+                  disabled={!canEditSettings || loadingDashboardSettings[setting.id]}
+                  id={setting.id}
+                  checked={setting.value === "true"}
+                  onCheckedChange={(checked) => {
+                    updateDashboardSetting(setting.id, checked.toString());
+                  }}
+                />
+                <label className="select-none hover:cursor-pointer" htmlFor={setting.id}>
+                  {setting.title}
+                </label>
+                {loadingDashboardSettings[setting.id] && <Loader color="#000" />}
+                {!canEditSettings && <LockKeyhole className="text-muted-foreground h-3.5 w-3.5" />}
+              </div>
+            ))}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
