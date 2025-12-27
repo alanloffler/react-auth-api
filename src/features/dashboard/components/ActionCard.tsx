580c889abb0a6a704d89bb603a55f64ceb628af8
alanloffler <alanmatiasloffler@gmail.com>
2025-12-27 18:54:37 -0300

    feat(dashboard): plain or card style

--- src/features/dashboard/components/ActionCard.tsx
+++ src/features/dashboard/components/ActionCard.tsx
@@ -6,20 +6,28 @@ import type { ComponentType, SVGProps } from "react";
 import type { LucideIcon } from "lucide-react";
 
 interface IProps {
+  asCard?: boolean;
   icon: LucideIcon | ComponentType<SVGProps<SVGSVGElement>>;
   permission: string;
   text: string;
   url: string;
 }
 
-export function ActionCard({ icon: Icon, permission, text, url }: IProps) {
+export function ActionCard({ asCard = true, icon: Icon, permission, text, url }: IProps) {
   return (
     <Protected requiredPermission={permission}>
       <Link className="col-span-2" to={url}>
-        <Card className="items-center gap-3 px-6 font-medium hover:bg-neutral-50">
-          <Icon className="h-6 w-6" />
-          <span>{text}</span>
-        </Card>
+        {asCard ? (
+          <Card className="items-center gap-3 px-6 font-medium transition-colors duration-150 hover:bg-neutral-50">
+            <Icon className="h-6 w-6" />
+            <span>{text}</span>
+          </Card>
+        ) : (
+          <div className="justify-left flex h-full items-center gap-3 rounded-xl p-6 font-medium transition-colors duration-150 hover:bg-neutral-100">
+            <Icon className="h-6 w-6" />
+            <span>{text}</span>
+          </div>
+        )}
       </Link>
     </Protected>
   );
