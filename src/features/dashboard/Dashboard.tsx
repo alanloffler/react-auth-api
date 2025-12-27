3e8b16d87603f855e8c3d714f186ed2b80747ba2
alanloffler <alanmatiasloffler@gmail.com>
2025-12-27 19:02:38 -0300

    feat(dashboard): settings integration

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,37 +1,78 @@
 import { KeyRound, LayoutDashboard, Package, Shield, ShieldPlus, UserRoundPlus, UsersRound } from "lucide-react";
 
 import { ActionCard } from "@dashboard/components/ActionCard";
+import { Card } from "@components/ui/card";
 import { ConfigButton } from "@dashboard/components/ConfigButton";
 import { KeyRoundPlus } from "@components/icons/KeyRoundPlus";
 import { PageHeader } from "@components/pages/PageHeader";
 
+import { useAuthStore } from "@auth/auth.store";
 import { usePermission } from "@core/hooks/usePermission";
+import { useSettingsStore } from "@settings/stores/settings.store";
 
 export default function Dashboard() {
   const showAdmin = usePermission(["admin-view", "admin-create"], "some");
   const showPermissions = usePermission(["permissions-view", "permissions-create"], "some");
   const showRoles = usePermission(["roles-view", "roles-create"], "some");
+  const user = useAuthStore((state) => state.admin);
+  const { dashboardSettings } = useSettingsStore();
+
+  const showConfigButtons = dashboardSettings.find((setting) => setting.key === "showConfigButtons")?.value === "true";
+  const showHelloMessage = dashboardSettings.find((setting) => setting.key === "showHelloMessage")?.value === "true";
 
   return (
     <div className="flex flex-col gap-10">
+      {showHelloMessage && (
+        <Card className="bg-accent mx-auto w-full gap-2 p-6 text-center lg:w-1/2">
+          <h1 className="text-2xl font-semibold">
+            Hola, {user?.firstName} {user?.lastName}
+          </h1>
+          <p className="text-lg font-medium">Bienvenido a tu panel de control</p>
+        </Card>
+      )}
       <PageHeader title="Panel de control" subtitle="Administra tu aplicación" />
       <div className="flex flex-col gap-8">
         {showAdmin && (
           <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
-            <ActionCard icon={UsersRound} permission="admin-view" text="Administradores" url="/admin" />
-            <ActionCard icon={UserRoundPlus} permission="admin-create" text="Crear Administrador" url="/admin/create" />
+            <ActionCard
+              asCard={showConfigButtons}
+              icon={UsersRound}
+              permission="admin-view"
+              text="Administradores"
+              url="/admin"
+            />
+            <ActionCard
+              asCard={showConfigButtons}
+              icon={UserRoundPlus}
+              permission="admin-create"
+              text="Crear Administrador"
+              url="/admin/create"
+            />
           </div>
         )}
         {showRoles && (
           <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
-            <ActionCard icon={Shield} permission="roles-view" text="Roles" url="/roles" />
-            <ActionCard icon={ShieldPlus} permission="roles-create" text="Crear rol" url="/roles/create" />
+            <ActionCard asCard={showConfigButtons} icon={Shield} permission="roles-view" text="Roles" url="/roles" />
+            <ActionCard
+              asCard={showConfigButtons}
+              icon={ShieldPlus}
+              permission="roles-create"
+              text="Crear rol"
+              url="/roles/create"
+            />
           </div>
         )}
         {showPermissions && (
           <div className="grid grid-cols-4 gap-8 lg:grid-cols-6 xl:grid-cols-8">
-            <ActionCard icon={KeyRound} permission="permissions-view" text="Permisos" url="/permissions" />
             <ActionCard
+              asCard={showConfigButtons}
+              icon={KeyRound}
+              permission="permissions-view"
+              text="Permisos"
+              url="/permissions"
+            />
+            <ActionCard
+              asCard={showConfigButtons}
               icon={KeyRoundPlus}
               permission="permissions-create"
               text="Crear permiso"
@@ -39,17 +80,19 @@ export default function Dashboard() {
             />
           </div>
         )}
-        <div className="flex flex-col gap-3">
-          <h1 className="font-semibold">Configuraciones</h1>
-          <ul className="flex gap-3 pl-5">
-            <li>
-              <ConfigButton icon={Package} text="Aplicación" url="/app-settings" />
-            </li>
-            <li>
-              <ConfigButton icon={LayoutDashboard} text="Tablero" url="/dashboard-settings" />
-            </li>
-          </ul>
-        </div>
+        {showConfigButtons && (
+          <div className="flex flex-col gap-3">
+            <h1 className="font-semibold">Configuraciones</h1>
+            <ul className="flex gap-3 pl-5">
+              <li>
+                <ConfigButton icon={Package} text="Aplicación" url="/app-settings" />
+              </li>
+              <li>
+                <ConfigButton icon={LayoutDashboard} text="Tablero" url="/dashboard-settings" />
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
