0e5b466d7128071d5cb3d6f340a53c4c2a9e9c88
alanloffler <alanmatiasloffler@gmail.com>
2025-12-28 20:46:03 -0300

    refactor(admin): remove loader color

--- src/features/admin/components/CreateForm.tsx
+++ src/features/admin/components/CreateForm.tsx
@@ -343,7 +343,7 @@ export function CreateForm() {
         </form>
       </CardContent>
       <CardFooter className="flex items-center justify-between pt-4">
-        <div>{isLoadingRoles && <Loader className="text-sm" color="black" size={18} text="Cargando roles" />}</div>
+        <div>{isLoadingRoles && <Loader className="text-sm" size={18} text="Cargando roles" />}</div>
         <div className="flex gap-4">
           <Button variant="ghost" onClick={resetForm}>
             Cancelar
