01ff6a2ac3e15dba83b09572eaa92d5c14196bc1
alanloffler <alanmatiasloffler@gmail.com>
2025-12-28 19:08:00 -0300

    styles(roles): dark styles

--- src/features/roles/components/EditForm.tsx
+++ src/features/roles/components/EditForm.tsx
@@ -184,7 +184,7 @@ export function EditForm() {
             <FieldGroup>
               <div
                 className={cn(
-                  "flex flex-col gap-5 rounded-md border border-gray-200 p-6",
+                  "flex flex-col gap-5 rounded-md border p-6",
                   form.formState.errors.permissions && "border-destructive",
                 )}
               >
