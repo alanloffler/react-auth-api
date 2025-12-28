56ea4447faab27eb913a76f9fe492c59bd3a9133
alanloffler <alanmatiasloffler@gmail.com>
2025-12-28 18:51:51 -0300

    styles(components): dark styles

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -10,12 +10,12 @@ const badgeVariants = cva("inline-flex items-center justify-center font-medium u
     },
     variant: {
       cyan: "bg-cyan-100 text-cyan-600",
-      ic: "bg-neutral-200 text-neutral-700 text-xs lowercase font-normal",
-      id: "bg-neutral-100 text-neutral-600 text-xs lowercase font-normal",
+      ic: "dark:bg-neutral-700 dark:text-neutral-200 bg-neutral-200 text-neutral-700 text-xs lowercase font-normal",
+      id: "dark:bg-neutral-600 dark:text-neutral-100 bg-neutral-100 text-neutral-600 text-xs lowercase font-normal",
       gray: "bg-neutral-100 text-neutral-600",
       green: "bg-green-100 text-green-600 ",
       red: "bg-red-100 text-red-600",
-      role: "bg-indigo-100 text-indigo-600",
+      role: "dark:bg-indigo-600/30 dark:text-indigo-300 bg-indigo-100 text-indigo-600",
     },
   },
   defaultVariants: {
