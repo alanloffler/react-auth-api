c7d443d0629fbdc947ce3a1b8a74193162d1027a
alanloffler <alanmatiasloffler@gmail.com>
2025-12-30 17:02:09 -0300

    fix(layouts): keep same header height on sidebar closed

--- src/features/layouts/MainLayout.tsx
+++ src/features/layouts/MainLayout.tsx
@@ -10,7 +10,7 @@ export function MainLayout() {
     <SidebarProvider className="h-full overflow-hidden">
       <AppSidebar />
       <SidebarInset className="flex h-full flex-col overflow-hidden">
-        <header className="light:shadow-lg light:shadow-white z-50 flex h-16 shrink-0 items-center justify-between gap-2 border-b pr-3 pl-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="light:shadow-lg light:shadow-white z-50 flex h-16 shrink-0 items-center justify-between gap-2 border-b pr-3 pl-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-16">
           <div className="flex items-center gap-2">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
