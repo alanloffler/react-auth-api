a2c55f61dd9b7c0b7bd21efa1b5992d97e3f7dc3
alanloffler <alanmatiasloffler@gmail.com>
2025-12-30 22:28:25 -0300

    fix(admin): auth store import alias

--- src/features/admin/Admin.tsx
+++ src/features/admin/Admin.tsx
@@ -17,7 +17,7 @@ import type { IAdmin } from "@admin/interfaces/admin.interface";
 import { AdminService } from "@admin/services/admin.service";
 import { ERoles } from "@auth/enums/role.enum";
 import { tryCatch } from "@core/utils/try-catch";
-import { useAuthStore } from "@auth/auth.store";
+import { useAuthStore } from "@auth/stores/auth.store";
 import { useSidebar } from "@components/ui/sidebar";
 import { useTryCatch } from "@core/hooks/useTryCatch";
 
