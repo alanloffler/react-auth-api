93461e3bc5c7628460b1f530602e34149578bce8
alanloffler <alanmatiasloffler@gmail.com>
2026-01-01 13:16:05 -0300

    feat(components): global search filter

--- src/core/components/data-table/DataTable.tsx
+++ src/core/components/data-table/DataTable.tsx
@@ -1,11 +1,16 @@
+import { Search, X } from "lucide-react";
+
+import { Input } from "@components/ui/input";
 import { Pagination } from "@components/Pagination";
 import { Skeleton } from "@components/ui/skeleton";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@components/ui/table";
 
+import { Activity, useMemo, useState } from "react";
 import {
   type ColumnDef,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   type PaginationState,
@@ -14,7 +19,6 @@ import {
 } from "@tanstack/react-table";
 
 import { cn } from "@lib/utils";
-import { useMemo, useState } from "react";
 
 interface DataTableProps<TData, TValue> {
   className?: string;
@@ -41,6 +45,7 @@ export function DataTable<TData, TValue>({
     pageIndex: 0,
     pageSize: defaultPageSize,
   });
+  const [globalFilter, setGlobalFilter] = useState<string>("");
   const [sorting, setSorting] = useState<SortingState>(defaultSorting);
 
   const tableData = useMemo(() => (loading ? Array(5).fill({}) : data), [loading, data]);
@@ -60,19 +65,44 @@ export function DataTable<TData, TValue>({
     data: tableData || [],
     columns: tableColumns,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    globalFilterFn: "includesString",
+    onGlobalFilterChange: setGlobalFilter,
     onPaginationChange: setPagination,
     onSortingChange: setSorting,
     state: {
       columnVisibility: columnVisibility,
+      globalFilter: globalFilter,
       pagination: pagination,
       sorting: sorting,
     },
   });
 
+  function handleClearSearch(): void {
+    table.setGlobalFilter("");
+    setGlobalFilter("");
+  }
+
   return (
     <div className={cn("overflow-hidden rounded-md border shadow-sm", className)}>
+      <div className="flex w-full items-center justify-end p-3">
+        <div className="relative">
+          <Search className="stroke-primary absolute top-1/2 left-5 h-4 w-4 -translate-x-1/2 -translate-y-1/2" />
+          <Input
+            value={globalFilter}
+            className="w-55 pl-9"
+            onChange={(e) => table.setGlobalFilter(String(e.target.value))}
+            placeholder="Buscar..."
+          />
+          <Activity mode={globalFilter ? "visible" : "hidden"}>
+            <button className="text-muted-foreground hover:text-foreground absolute top-1/2 -right-1.5 -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full p-1 transition-colors duration-100">
+              <X className="h-4 w-4" onClick={handleClearSearch} />
+            </button>
+          </Activity>
+        </div>
+      </div>
       <Table className="dark:bg-muted">
         <TableHeader className="dark:bg-primary-foreground bg-neutral-100">
           {table.getHeaderGroups().map((headerGroup) => (
